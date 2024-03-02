c6621103a33d1a655044586c26cf550aaf343f67
ardelvito <ardelvito8738@gmail.com>
2024-03-02 14:48:51 +0700

    First Commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,46 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link"
+                            href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
+                    </li>
+                    <li class="">
+                        <a class="nav-link" href="{{ route('users.index') }}">Users</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu-header">Starter</li>
+            <li class="{{ Request::is('blank-page') ? 'active' : '' }}">
+                <a class="nav-link"
+                    href="{{ url('blank-page') }}"><i class="far fa-square"></i> <span>Blank Page</span></a>
+            </li>
+            <li class="menu-header">Stisla</li>
+            <li class="menu-header">Pages</li>
+            <li class="{{ Request::is('credits') ? 'active' : '' }}">
+                <a class="nav-link"
+                    href="{{ url('credits') }}"><i class="fas fa-pencil-ruler">
+                    </i> <span>Credits</span>
+                </a>
+            </li>
+        </ul>
+
+        <div class="hide-sidebar-mini mt-4 mb-4 p-3">
+            <a href="https://getstisla.com/docs"
+                class="btn btn-primary btn-lg btn-block btn-icon-split">
+                <i class="fas fa-rocket"></i> Documentation
+            </a>
+        </div>
+    </aside>
+</div>
